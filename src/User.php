9a26a65e5e17e553c0da4f8fa751db430934fdf4
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-02-11 10:00:22 -0300

    add fields hqpilot npomember connection seconds

--- src/User.php
+++ src/User.php
@@ -29,7 +29,11 @@ class User implements \Laravel\Socialite\Contracts\User, ArrayAccess
             "pilotRating" => $raw["ratingpilot"],
             "division" => $raw["division"],
             "country" => $raw["country"],
-            "staff" => $raw["staff"] ? explode(":", $raw["staff"]) : []
+            "staff" => $raw["staff"] ? explode(":", $raw["staff"]) : [],
+            "isNpoMember" => $raw["isNpoMember"] != 0,
+            "isHqPilot" => $raw["hq_pilot"] != 0,
+            "seconds_as_pilot" => $raw["hours_pilot"],
+            "seconds_as_atc" => $raw["hours_atc"],
         ];
     }
 
